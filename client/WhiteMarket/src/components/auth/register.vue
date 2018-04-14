a00a6b382e15dba060242e623e1ec1883792a113
oscll <oscllweb@gmail.com>
2018-04-14 14:07:27 +0200

    geolocation gmaps

--- client/WhiteMarket/src/components/auth/register.vue
+++ client/WhiteMarket/src/components/auth/register.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-4 my-2 pb-3">
-                <div class="w-100 my-5">
+        <form class="row my-5">
+            <div class="col-sm-4 my-2 pb-3 col-xs-12">
+                <div class="w-100 mb-5">
                     <label for="username">Username</label>
                     <div class="input-group">
                         <div class="input-group-prepend"><span class="input-group-text">$</span></div>
                         <input type="text" class="form-control" required id="username" placeholder="Username" v-model="username">
                     </div>
                 </div>
-                <div class="w-100 my-5">
+                <div class="w-100 mb-5">
                     <label for="email">Email</label>
                     <div class="input-group">
                         <div class="input-group-prepend"><span class="input-group-text">$</span></div>
                         <input type="email" class="form-control" required id="email" placeholder="example@whitemarke.xyz" v-model="email">
                     </div>
                 </div>
-                <div class="w-100 my-5">
+                <div class="w-100 mb-5">
                     <label for="password">Password</label>
                     <div class="input-group">
                         <div class="input-group-prepend"><span class="input-group-text">$</span></div>
@@ -24,10 +24,11 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-8 my-2 pb-3" id="map">
-                
+            <div class="col-sm-8 col-xs-12 my-2 pb-3" id="map">
+                <input type="hidden" required id="latitude" v-model="latitude" > 
+                <input type="hidden" required id="longitude" v-model="longitude" > 
             </div>
-        </div>
+        </form>
     </div>
 </template>
 
@@ -38,57 +39,65 @@ export default {
             email:null,
             password:null,
             username:null,
-            lat:null,
-            long:null
+            latitude:null,
+            longitude:null
         }
     },
     methods:{
         initMap: function() {
         // Create a map object and specify the DOM element for display.
-        google.maps.visualRefresh = true;
+            google.maps.visualRefresh = true;
 
-        var infowindow = null;
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: new google.maps.LatLng(39.9078, 32.8252),
-            zoom: 10,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
+            var infowindow = null;
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: new google.maps.LatLng(this.latitude, this.longitude),
+                zoom: 10,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
             google.maps.event.addListener(map, 'click', (e) => {
-                        this.lat = e.latLng.lat();
-                        this.long = e.latLng.lng();
-                        if (infowindow != null)
-                            infowindow.close();
+                this.latitude = e.latLng.lat();
+                this.longitude = e.latLng.lng();
+                if (infowindow != null)
+                    infowindow.close();
 
-                        infowindow = new google.maps.InfoWindow({
-                            content: '<b>Mouse Coordinates : </b><br><b>Latitude : </b>' + e.latLng.lat() + '<br><b>Longitude: </b>' + e.latLng.lng(),
-                            position: e.latLng
-                        });
-                        infowindow.open(map);
-                    });
-      },
+                infowindow = new google.maps.InfoWindow({
+                    content: '<b>Mouse Coordinates : </b><br><b>Latitude : </b>' + e.latLng.lat() + '<br><b>Longitude: </b>' + e.latLng.lng(),
+                    position: e.latLng
+                });
+                infowindow.open(map);
+            });
+        },
         createGoogleMaps: function() {
             return new Promise((resolve, reject) => {
-            let gmap = document.createElement('script');
-            gmap.src = `https://maps.googleapis.com/maps/api/js?key=`;
-            gmap.type = 'text/javascript';
-            gmap.onload = resolve;
-            gmap.onerror = reject;
-            document.body.appendChild(gmap);
+                let gmap = document.createElement('script');
+                gmap.src = `https://maps.googleapis.com/maps/api/js?key=`;
+                gmap.type = 'text/javascript';
+                gmap.onload = resolve;
+                gmap.onerror = reject;
+                document.body.appendChild(gmap);
             })
         },
-        initGoogleMaps: function() {
-            // We would set up our options object beforehand or grab it from Redux in my case
-            this.vueGMap = new google.maps.Map(document.getElementById('map')); 
-        }
     },
 
     mounted() {
         this.createGoogleMaps().then(this.initMap, this.googleMapsFailedToLoad);
-    }
-
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((data)=>{
+            console.log(data);
+            console.log(this)
+            this.latitude = data.coords.latitude;
+            this.longitude = data.coords.longitude;
+            this.createGoogleMaps().then(this.initMap, this.googleMapsFailedToLoad);
+        });
+        }else{
+            console.log('No seas hipocrita')
+        }
+    },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#map{
+   min-height: 480px;
+}
 </style>
